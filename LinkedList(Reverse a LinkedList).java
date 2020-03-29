232d5dd215670fd941b8ffca6940a6b15d735af5
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-03-29 16:36:06 +0530

    Add files via upload

---
+++ LinkedList(Reverse a LinkedList).java
@@ -0,0 +1,54 @@
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+class LinkedList
+{
+	static Node head;
+
+	Node reverse(Node node)
+	{
+		Node prev = null; 
+        Node current = node; 
+        Node next = null; 
+        while (current != null) { 
+            next = current.next; 
+            current.next = prev; 
+            prev = current; 
+            current = next; 
+        } 
+        node = prev;
+        return node; 
+	}  
+
+	void printList(Node node)
+	{
+		Node temp = head;
+		while(temp!=null)
+		{
+			System.out.print(temp.data + "-->");
+			temp = temp.next;
+		}
+		System.out.print("null");
+	}
+
+	public static void main(String[] args)
+	{
+		LinkedList list = new LinkedList();
+		list.head = new Node(10);
+		list.head.next = new Node(20);
+		list.head.next.next = new Node(30);
+		list.head.next.next.next = new Node(40);
+		list.head.next.next.next.next = new Node(50);
+
+                head = list.reverse(head);
+		list.printList(head);
+	}
+}
