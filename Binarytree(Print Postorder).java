5be0f2929adb513087fce55835b8dd2fe2921806
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-04-14 15:55:16 +0530

    Add files via upload

---
+++ Binarytree(Print Postorder).java
@@ -0,0 +1,49 @@
+class Node
+{
+	int data;
+	Node left,right;
+	Node(int d)
+	{
+		data = d;
+		left = right = null;
+	}
+}
+
+class BinaryTree
+{
+	static Node root;
+
+	BinaryTree()
+	{
+		root = null;
+	}
+
+	BinaryTree(int x)
+	{
+		root = new Node(x);
+	}
+
+	void printPostOrder(Node root)
+	{
+		if(root == null)
+			return ;
+
+		printPostOrder(root.left);
+
+		printPostOrder(root.right);
+
+		System.out.print(root.data + " ");
+	}
+
+	public static void main(String[] args)
+	{
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(4); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(5); 
+        tree.root.left.left = new Node(1); 
+        tree.root.left.right = new Node(3); 
+
+        tree.printPostOrder(root);
+	}
+}
