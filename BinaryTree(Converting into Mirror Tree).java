b108f0936b044e17018aff4a9fcf7438b6ae9465
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-05-02 14:09:46 +0530

    Update BinaryTree(Converting into Mirror Tree).java

--- BinaryTree(Converting into Mirror Tree).java
+++ BinaryTree(Converting into Mirror Tree).java
@@ -40,9 +40,9 @@ class BinaryTree
 
 	public static void main(String[] args)
 	{
-		BinaryTree tree = new BinaryTree();
-		tree.root = new Node(4); 
-        	tree.root.left = new Node(2); 
+		     BinaryTree tree = new BinaryTree();
+		     tree.root = new Node(4); 
+        	     tree.root.left = new Node(2); 
         	tree.root.right = new Node(5); 
         	tree.root.left.left = new Node(1); 
         	tree.root.left.right = new Node(3); 
